26c7cb1cc134fef35cc9e553f22ae58c54976a64
Vladislav <Vladislav_lebedkin@mail.ru>
2023-05-23 20:29:36 +0300

    []01-02][сделал header and footer]

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/Footer.module.css";
+
+import { ROUTES } from "../../utils/routes";
+
+import LOGO from "../../images/logo.svg";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt="Stuff" />
+        </Link>
+      </div>
+      <div className={styles.rights}>Developed by Vladi</div>
+      <div className={styles.socials}>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+          </svg>
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noreferrer">
+          <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+          </svg>
+        </a>
+        <a href="https://youtube.com" target="_blank" rel="noreferrer">
+          <svg className="icon">
+            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+          </svg>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from "../../styles/Sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.title}>
+        
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar
